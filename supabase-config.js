c6ac11690b8222d393a9913b452d7bf57b993aa9
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-07-05 15:28:13 -0500

    Use anon public Supabase key for auth

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.MESACARDS_SUPABASE = {
   url: 'https://hbiqjewiioieolmkwvax.supabase.co',
-  key: 'sb_publishable_Dp4tIlxH-VOk6cb7zQN3Qg_2jRhh1O4'
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaXFqZXdpaW9pZW9sbWt3dmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjc4MjYsImV4cCI6MjA5ODg0MzgyNn0.qNq1-3njyVQ5WW3OSxq4SZ9FZS99oEVuks8TSc5XFi8'
 };
